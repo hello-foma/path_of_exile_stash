9bf7d7d2327007d92e7aba10d057b818dfc263ab
hello-foma <dev@foma.me>
2022-04-02 15:47:24 +0300

    feat(app): new route to login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from '../login-page/login-page.component';
 
-const routes: Routes = [];
+export const AppRoutes = {
+  login: 'login'
+};
+
+const routes: Routes = [
+  {
+    path: AppRoutes.login,
+    component: LoginPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: AppRoutes.login
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginPageModule } from '../login-page/login-page.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    LoginPageModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
