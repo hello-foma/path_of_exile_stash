77ac508425075140863256b942161e837e4a8c73
hello-foma <dev@foma.me>
2022-04-02 15:29:13 +0300

    refactor(shared): rename page-layout component

--- src/shared/page-layout/page-layout.component.ts
+++ src/shared/page-layout/page-layout.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-page-layout',
+  selector: 'pes-page-layout',
   templateUrl: './page-layout.component.html',
   styleUrls: ['./page-layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
