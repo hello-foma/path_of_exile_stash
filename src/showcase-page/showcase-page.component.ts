34f1f9126c400829883d03ff7e03c21c158968df
hello-foma <dev@foma.me>
2022-04-03 00:44:00 +0300

    feat(showcase): first draft of interface

--- src/showcase-page/showcase-page.component.ts
+++ src/showcase-page/showcase-page.component.ts
@@ -1,4 +1,5 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Item } from 'src/api/item.type';
 
 @Component({
   selector: 'pes-showcase-page',
@@ -7,6 +8,8 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ShowcasePageComponent implements OnInit {
+
+  public items: Item[] = [];
 
   constructor() { }
 
--- src/showcase-page/showcase-page.module.ts
+++ src/showcase-page/showcase-page.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShowcasePageComponent } from './showcase-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SharedModule } from 'src/shared/shared.module';
 
 const routes: Routes = [
   {
@@ -16,7 +17,8 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forChild(routes),
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class ShowcasePageModule { }
