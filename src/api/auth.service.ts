71e568279fd66cdba780cca43b732fd596dc124b
hello-foma <dev@foma.me>
2022-04-02 17:10:10 +0300

    chore(api): get actual accountName data

--- src/api/auth.service.ts
+++ src/api/auth.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
+
+  /**
+   * Get possible login values
+   */
+  public async getLoginSuggest(): Promise<string[]> {
+    const stashes = await this.api.getFirstStash();
+    const logins = new Map<string, boolean>();
+    stashes.stashes.forEach((stash) => logins.set(stash.accountName, true));
+
+    return Array.from(logins.keys());
+  }
 }
