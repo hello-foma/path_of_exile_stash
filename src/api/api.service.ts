dd10e07e06fb2fcb74b2c381e7639a462a9b44f0
hello-foma <dev@foma.me>
2022-04-03 01:00:12 +0300

    chore(api): get user's stash method

--- src/api/api.service.ts
+++ src/api/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { ApiResponse } from './api-response.type';
+import { Stash } from 'src/api/stash.type';
 
 @Injectable()
 export class ApiService {
@@ -17,6 +18,15 @@ export class ApiService {
     const query = '_limit=1';
     const path = environment.apiUrl + '?' + query;
 
+    // todo: filter corrupted data
     return ApiService.get<ApiResponse[]>(path).then(data => data[0]);
+  }
+
+  public async getStashForUser(userName: string): Promise<Stash | null> {
+    const stashes = await this.getFirstStash();
+
+    const userStash = stashes.stashes.find((stash) => stash.accountName === userName);
+
+    return userStash || null;
   }
 }
