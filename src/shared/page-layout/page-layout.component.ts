96e66b18ced09a67ef1f1aea1a4c5e01d1cf42ed
hello-foma <dev@foma.me>
2022-04-02 15:27:47 +0300

    chore(shared): create page-layout component

---
+++ src/shared/page-layout/page-layout.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-layout',
+  templateUrl: './page-layout.component.html',
+  styleUrls: ['./page-layout.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PageLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PageLayoutComponent } from './page-layout/page-layout.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageLayoutComponent
+  ],
   imports: [
     CommonModule
   ]
