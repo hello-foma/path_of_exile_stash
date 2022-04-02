6144cb3e3e014d9d706d7022097a68817cfe9fd1
hello-foma <dev@foma.me>
2022-04-02 16:45:17 +0300

    chore(api): create api service

---
+++ src/api/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+
+@Injectable()
+export class ApiService {
+
+  constructor() { }
+
+  private get<T>(query: string): Promise<T> {
+    // todo: error handling
+    return fetch(query)
+      .then(response => response.json())
+  }
+
+  public getFirstStash(): Promise<any> {
+    const query = '_limit=1';
+    const path = environment.apiUrl + '?' + query;
+
+    return this.get(query);
+  }
+}
